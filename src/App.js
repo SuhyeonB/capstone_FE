5834ab21dafda1dc1573248316cc1848a4fca841
SuhyeonB <peridot489@gmail.com>
2024-08-21 15:38:50 +0900

    update file structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import ScrollToTop from "./utils/ScrollTolTop";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <BrowserRouter>
+          <Header/>
+          <ScrollToTop />
+          <Routes>
+            <Route path='/' element={<Main/>}/>
+          </Routes>
+          <Footer/>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Main.css'
+
+const Main = () => {
+    return (
+        <div class="wallpaper">
+            <div className='text'>
+            <strong>당신</strong>의 <br/>오늘 하루는 어땠나요?
+            </div>
+        </div>
+    );
+};
+
+export default Main;
