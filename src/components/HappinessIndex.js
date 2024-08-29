66a3b6684632264df007a6d2c42e28833f52dd26
Choi-Seung-Mi <choism0552@naver.com>
2024-08-29 15:53:17 +0900

    add HappinessIndex & Calendar

---
+++ src/components/HappinessIndex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HappinessIndex = () => {
+  return (
+    <div className="happiness-index-container">
+      <h2>행복지수</h2>
+    </div>
+  );
+};
+
+export default HappinessIndex;
