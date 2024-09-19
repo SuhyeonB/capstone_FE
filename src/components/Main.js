6f88a0ab85d03ab8a186fffebef529713cfe1b68
Choi-Seung-Mi <choism0552@naver.com>
2024-09-19 12:48:53 +0900

    temp

--- src/components/Main.js
+++ src/components/Main.js
@@ -40,19 +40,3 @@ const Main = () => {
 };
 
 export default Main;
-
-
-/*import React from 'react';
-import '../styles/component_css/Main.css'
-
-const Main = () => {
-    return (
-        <div className="wallpaper">
-            <div className='text fade-in-up'>
-            <strong>당신</strong>의 <br/>오늘 하루는 어땠나요?
-            </div>
-        </div>
-    );
-};
-
-export default Main;*/
