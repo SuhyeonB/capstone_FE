bf52fc41c06fde36fc6852c159d0fb82948518f5
Choi-Seung-Mi <choism0552@naver.com>
2024-09-30 20:55:51 +0900

    add Editprofile.js & update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/component_css/Main.css"
+import "../styles/component_css/Main.css";
 
 const Header = () => {
-    return (
-        <header className="header">
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); 
+  const dropdownRef = useRef(null);
+
+  // 드롭다운 토글 함수
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    alert("로그아웃 되었습니다.");
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    // 외부 클릭시 드롭다운 메뉴 닫기
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <header className="header">
       <div className="header-left">
         <Link to="/" className="brand">My Daily Diary</Link>
         <ul className="nav-links">
@@ -14,10 +42,26 @@ const Header = () => {
         </ul>
       </div>
       <div className="header-right">
-        <Link to="/signin" className="login">로그인</Link>
+        {isLoggedIn ? (
+          <>
+            <button className="logout" onClick={handleLogout}>로그아웃</button>
+            <li className="mypage-container" ref={dropdownRef}>
+              <button className="mypage" onClick={toggleDropdown}>마이페이지</button>
+              {isDropdownOpen && (
+                <ul className="dropdown-menu">
+                  <li><Link to="/likes">좋아요 보관함</Link></li>
+                  <li><Link to="/editprofile">회원정보수정</Link></li>
+                  <li><Link to="/deleteaccount">회원탈퇴</Link></li>
+                </ul>
+              )}
+            </li>
+          </>
+        ) : (
+          <Link to="/signin" className="login">로그인</Link>
+        )}
       </div>
     </header>
-    );
+  );
 };
 
 export default Header;
