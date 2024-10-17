1a9a529a5ddeab5673d0e3d3242fa2d7189eda78
Choi-Seung-Mi <choism0552@naver.com>
2024-10-17 14:57:11 +0900

    cash clear

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,28 +1,36 @@
 import React, { useState } from 'react';
-import '../styles/Sign.css'
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import '../styles/Sign.css'
+import dummy_user from '../dummy/dummy_user';
 
 function Signin() {
-  // id와 pw를 관리할 상태 변수.
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
+  const navigate = useNavigate(); // 페이지 이동을 위한 네비게이션 훅
 
-  // 로그인 버튼 클릭 시 실행되는 함수.
   const handleLoginClick = () => {
     if (!id && !pw) {
-      alert('아이디와 비밀번호를 모두 입력하세요.');
+      alert('이메일과 비밀번호를 모두 입력하세요.');
     } else if (!id) {
-      alert('아이디를 입력하세요.');
+      alert('이메일을 입력하세요.');
       resetFields();
     } else if (!pw) {
       alert('비밀번호를 입력하세요.');
       resetFields();
     } else {
-      alert(`로그인이 성공적으로 처리되었습니다.`);
+      const user = dummy_user.find(user => user.email === id && user.password === pw);
+
+      if (user) {
+        alert(`${user.name}님, 로그인에 성공했습니다.`);
+        navigate('/'); // 로그인 성공 시 메인 페이지로 리다이렉트
+      } else {
+        alert('이메일 또는 비밀번호가 일치하지 않습니다.');
+        resetFields();
+      }
     }
   }
 
-  // id, pw 초기화.
   const resetFields = () => {
     setId('');
     setPw('');
@@ -41,11 +49,11 @@ function Signin() {
         <form className="signin-form">
           <input 
             type="text" 
-            placeholder="아이디" 
+            placeholder="이메일" 
             className="signin-input" 
             value={id} 
             onChange={(e) => setId(e.target.value)} 
-            onKeyDown={handleKeyDown} // ENTER 입력 감지.
+            onKeyDown={handleKeyDown} // ENTER 입력 감지
           />
           <input 
             type="password" 
@@ -53,25 +61,26 @@ function Signin() {
             className="signin-input" 
             value={pw} 
             onChange={(e) => setPw(e.target.value)} 
-            onKeyDown={handleKeyDown} // ENTER 입력 감지.
+            onKeyDown={handleKeyDown} // ENTER 입력 감지
           />
           <button 
+            type="button"
             className="signin-button" 
             onClick={handleLoginClick}
           >
             로그인
           </button>
           <div className="signin-options">
-              <label>
+            <label>
               <input type="checkbox" /> 자동 로그인
             </label>
             <Link to='/signup' className="link-button">회원가입</Link>
           </div>
           <button className="kakao-button">카카오 로그인</button>
           <button className="naver-button">네이버 로그인</button>
-          </form>
-        </div>
+        </form>
       </div>
+    </div>
   );
 }
 
